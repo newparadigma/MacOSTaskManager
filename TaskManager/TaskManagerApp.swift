10f62b53ac5000198e10f63b4cd27d7d8cd1333c
Ivan Yashin <newparadigma1@gmail.com>
2024-03-25 20:48:39 +0700

    Initial Commit

---
+++ TaskManager/TaskManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskManagerApp.swift
+//  TaskManager
+//
+//  Created by Ivan Iashin on 25/3/24.
+//
+
+import SwiftUI
+
+@main
+struct TaskManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
